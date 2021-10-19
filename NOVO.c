89a45c87cac64ac01b94220850687bde23155219
Pedro Pinel <48687502+pinelpedro@users.noreply.github.com>
2021-10-19 10:17:23 -0300

    Novo02.c
    Falta aplicar as funções errorMSG() e okARGC(), alem de comentar o codico

--- NOVO.c
+++ NOVO.c
@@ -14,39 +14,155 @@
 #include <string.h>
 #include <ctype.h>
 
-const int MD = 1; // Selecionador de modo PLAY(0) / CORR(1)
+const int MD = 0; // Selecionador de modo PLAY(0) / CORR(1)
+
+
+void initOcean(char ocean[6][6]);
+void showOcean(char ocean[6][6]);
+void showInventory (int d, int s, int t);
+void submarinesIntoOcean (char ocean[6][6]);
+
+
+
+int
+main()
+{
+
+    char ocean[6][6];
+    int d = 0;
+    int s = 3;
+    int t = 6;
+
+    int i = 0;
+    int j = 0;
+    char tentativa[2];
+    int ativo =1;
+    int coluna = 0;
+    int fileira = 0;
+
+    initOcean(ocean);
+    for(i=1;i<=s;i++)
+    {
+        submarinesIntoOcean (ocean);
+    }
+
+    while(ativo!=0)
+    {
+        showOcean(ocean);
+        showInventory (d,s,t);
+
+        printf("onde deseja soltar o torpedo?  ");
+        scanf("%s",&tentativa);
+
+        coluna = tentativa[1] - '0';
+
+        if(tentativa[0]=='A')
+            {
+                fileira = 1;
+            }
+        if(tentativa[0]=='B')
+            {
+                fileira = 2;
+            }
+        if(tentativa[0]=='C')
+            {
+                fileira = 3;
+            }
+        if(tentativa[0]=='D')
+            {
+                fileira = 4;
+            }
+        if(tentativa[0] == 'E')
+            {
+                fileira = 5;
+            }
+
+        for(i=1;i<=5;i++)
+        {
+            for(j=1;j<=5;j++)
+                {
+                    if(ocean[i][j]=='S')
+                        {
+                            if(i== fileira && j == coluna)
+                                {
+                                    ocean[i][j] = '*';
+                                    s--;
+                                    t--;
+                                    printf("\nO torpedo acertou um submarino!\n");
+                                }
+                        }
+                    if(ocean[i][j]==' ')
+                        {
+                            if(i== fileira && j == coluna)
+                                {
+                                    t--;
+                                    printf("\nO torpedo afundou na agua \n");
+                                }
+                        }
+                  if(s <=0)
+                  {
+                      ativo = 0;
+                      printf("\nParabens, voce destruiu toda a frota!  \n");
+                      return 0;
+                  }
+                  if(t <= 0)
+                        {
+                            ativo = 0;
+                            printf("\nVoce ficou sem torpedos  \n");
+                            return 0;
+                        }
+
+                }
+        }
+
+
+    }
+
+    return 0;
+}
+
 
 
 void
-initOcean(char ocean[6][6])
+submarinesIntoOcean (char ocean[6][6])
 {
-    int i = 0;
-    int j = 0;
+    int loop = 0;
+    int X = rand() %5;
+    int Y = rand() %5;
+    while(X == 0 || Y == 0)
+    {
+         X = rand() %5;
+         Y = rand() %5;
+    }
 
+    printf("X:%d  Y:%d \n",X,Y);
 
-    for(i=1;i<=5;i++)
+    if(ocean[X+1][Y]=='S'&& X-1 != 0)
         {
-        for(j=1;j<=5;j++)
-            {
-            ocean[i][j] = ' ';
-
-            }
+            X--;
         }
-    ocean[0][0] = ' ';
+    if(ocean[X-1][Y]=='S'&& X+1 < 6)
+        {
+            X++;
+        }
+    if(ocean[X][Y+1]=='S'&& Y-1 != 0)
+        {
+            Y--;
+        }
+    if(ocean[X][Y-1]=='S'&& Y+1 < 6)
+        {
+            Y++;
+        }
 
-    ocean[1][0] = 'A';
-    ocean[2][0] = 'B';
-    ocean[3][0] = 'C';
-    ocean[4][0] = 'D';
-    ocean[5][0] = 'E';
+    ocean[X][Y] = 'S';
 
-    ocean[0][1] = '1';
-    ocean[0][2] = '2';
-    ocean[0][3] = '3';
-    ocean[0][4] = '4';
-    ocean[0][5] = '5';
+}
 
 
+void
+showInventory (int d, int s, int t)
+{
+    printf("\n\n Destruidores: %d\n Submarinos--: %d\n Torepedos---: %d\n",d,s,t);
 }
 
 
@@ -92,113 +208,33 @@ showOcean(char ocean[6][6])
 }
 
 void
-showInventory (int d, int s, int t)
+initOcean(char ocean[6][6])
 {
-    printf("\n\n Destruidores: %d\n Submarinos--: %d\n Torepedos---: %d\n",d,s,t);
-}
-
-void
-submarinesIntoOcean (char ocean[6][6])
-{
-    int loop = 0;
-    int X = rand() %5;
-    int Y = rand() %5;
-    while(X == 0 || Y == 0)
-    {
-         X = rand() %5;
-         Y = rand() %5;
-    }
-
-    printf("X:%d  Y:%d \n",X,Y);
-
-    if(ocean[X+1][Y]=='S'&& X-1 != 0)
-        {
-            X--;
-        }
-    if(ocean[X-1][Y]=='S'&& X+1 < 6)
-        {
-            X++;
-        }
-    if(ocean[X][Y+1]=='S'&& Y-1 != 0)
-        {
-            Y--;
-        }
-    if(ocean[X][Y-1]=='S'&& Y+1 < 6)
-        {
-            Y++;
-        }
-
-    ocean[X][Y] = 'S';
-
-}
-
-
-int
-main()
-{
-
-    char ocean[6][6];
-    int d = 0;
-    int s = 3;
-    int t = 6;
-
     int i = 0;
     int j = 0;
-    char tentativa[2];
-    int ativo =1;
-    int coluna = 0;
-    int fileira = 0;
 
-    initOcean(ocean);
-    for(i=1;i<=s;i++)
-    {
-        submarinesIntoOcean (ocean);
-    }
 
-    while(ativo!=0)
-    {
-        showOcean(ocean);
-        showInventory (d,s,t);
-
-        printf("onde deseja soltar o torpedo?");
-        scanf("%s",&tentativa);
-
-        coluna = tentativa[1] - '0';
-
-        if(tentativa[0]=='A')
-            {
-                fileira = 1;
-            }
-        if(tentativa[0]=='B')
-            {
-                fileira = 2;
-            }
-        if(tentativa[0]=='C')
-            {
-                fileira = 3;
-            }
-        if(tentativa[0]=='D')
-            {
-                fileira = 4;
-            }
-        if(tentativa[0] == 'E')
-            {
-                fileira = 5;
-            }
-
-        for(i=1;i<=5;i++)
+    for(i=1;i<=5;i++)
         {
-            for(j=1;j<=5;j++)
-                {
-                    if(ocean[i][j]=='S'&& fileira == i || coluna == j)
-                        {
-                            printf("funciona ");
-                        }
-                }
+        for(j=1;j<=5;j++)
+            {
+            ocean[i][j] = ' ';
+
+            }
         }
+    ocean[0][0] = ' ';
+
+    ocean[1][0] = 'A';
+    ocean[2][0] = 'B';
+    ocean[3][0] = 'C';
+    ocean[4][0] = 'D';
+    ocean[5][0] = 'E';
+
+    ocean[0][1] = '1';
+    ocean[0][2] = '2';
+    ocean[0][3] = '3';
+    ocean[0][4] = '4';
+    ocean[0][5] = '5';
 
 
-    }
-
-    return 0;
 }
