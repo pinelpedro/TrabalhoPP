826b8c16c1ce64857811fb924535fd03ae701824
Pedro Pinel <48687502+pinelpedro@users.noreply.github.com>
2021-10-23 14:07:40 -0300

    Create FINALMENTE.c

---
+++ FINALMENTE.c
@@ -0,0 +1,385 @@
+/*
+    Cria um jogo similar ao Batalha Naval. Nesse jogo, uma frota está escondida no oceano,
+    portanto, você não consegue perceber onde estão as naves que compõem a frota. O objetivo
+    do jogo é você destruir a frota, isto é, afundar todas as naves com a quantidade de torpedos
+    disponível.
+    por Jhully Vitoria, 2021
+    por Pedro Pinel, 2021
+    por Luis Arthur, 2021
+*/
+
+int MD = 0;
+
+const int O = 6;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+// -Protótipos-
+int argsOk(int argc, char *argv[]);
+void errorMsg(int x);
+void initOcean(char ocean[O][O]);
+void showOcean(char ocean[O][O]);
+void mantemOcean(char ocean[O][O]);
+void showInventory(int d, int s, int t);
+void submarinesIntoOcean(char ocean[O][O]);
+
+int
+main(int argc, char *argv[])
+{
+    char ocean[O][O]; // Oceano
+    int d = 0;        // Quantidade destroyers
+    int s = 3;        // Quantidade submarinos
+    int t = 0;        // Quantidades torpedos
+
+    int i = 0;
+    int j = 0;
+    char tentativa[2];  // Posição escolhida pelo jogador
+    int ativo = 1;      // Condição para manter o jogo
+    int coluna = 0;     // usado para converter a posição que o usuario digita na coluna
+    int fileira = 0;    // usado para converter a posição que o usuario digita na fileira
+    int r = 0;          // Valor do erro (argsOK)
+
+
+    if((r = argsOk(argc, argv)) != 0)
+    {
+        errorMsg(r);
+        return r;
+    }
+
+    if(strcmp(argv[1], "-t") == 0)
+    {
+        t = atoi(argv[2]);
+
+	if(strcmp(argv[4], "PLAY") == 0)
+	{
+            MD = 0;
+	}
+	else
+	{
+            MD = 1;
+	}
+    }
+    else
+    {
+        if(strcmp(argv[2], "PLAY") == 0)
+	{
+            MD = 0;
+	}
+	else
+	{
+            MD = 1;
+	}
+        t = atoi(argv[4]);
+    }
+
+    initOcean(ocean);
+
+    for(i = 1; i <= s; i++)
+    {
+        submarinesIntoOcean (ocean);
+    }
+
+    while(ativo != 0)
+    {
+        mantemOcean(ocean);
+        showOcean(ocean);
+        showInventory (d,s,t);
+
+        printf("onde deseja soltar o torpedo?  ");
+        scanf("%s", tentativa);
+
+        coluna = tentativa[1] - '0';
+
+        if(tentativa[0] =='A')
+        {
+            fileira = 1;
+        }
+        if(tentativa[0] =='B')
+        {
+            fileira = 2;
+        }
+        if(tentativa[0] =='C')
+        {
+            fileira = 3;
+        }
+        if(tentativa[0] =='D')
+        {
+            fileira = 4;
+        }
+        if(tentativa[0] == 'E')
+        {
+            fileira = 5;
+        }
+
+        for(i = 1; i <= 5; i++)
+        {
+            for(j = 1; j <= 5; j++)
+            {
+                if(ocean[i][j] == 'S')
+                {
+                    if (i == fileira && j == coluna)
+                    {
+                        ocean[i][j] = '*';
+                        s--;
+                        t--;
+                        printf("\nO torpedo acertou um submarino!\n");
+                    }
+                }
+                if (ocean[i][j] == ' ')
+                {
+                    if(i== fileira && j == coluna)
+                    {
+                        t--;
+                        ocean[i][j] = 'W';
+                        printf("\nO torpedo afundou na agua \n");
+                    }
+                }
+                if (s <= 0)
+                {
+                    ativo = 0;
+                    printf("\nParabens, voce destruiu toda a frota!  \n");
+                    return 0;
+                }
+                if (t <= 0)
+                {
+                    ativo = 0;
+                    printf("\nVoce ficou sem torpedos  \n");
+                    return 0;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+// --submarinesIntoOcean--
+// posiciona os submarinos no Oceano de forma pseudo-aleátoria
+
+void
+submarinesIntoOcean (char ocean[O][O])
+{
+    int X = rand() %5;
+    int Y = rand() %5;
+
+    while(X == 0 || Y == 0)
+    {
+        X = rand() %5;
+        Y = rand() %5;
+    }
+
+    if(ocean[X+1][Y] == 'S'&& X-1 != 0)
+    {
+        X--;
+    }
+    if(ocean[X-1][Y] == 'S'&& X+1 < O)
+    {
+        X++;
+    }
+    if(ocean[X][Y+1] == 'S'&& Y-1 != 0)
+    {
+        Y--;
+    }
+    if(ocean[X][Y-1]=='S'&& Y+1 < O)
+    {
+        Y++;
+    }
+
+    ocean[X][Y] = 'S';
+}
+
+// --showInventory--
+// apresenta a quantidade dedestruidores, submarinos e torpedos restantes para o jogador
+
+void
+showInventory (int d, int s, int t)
+{
+    printf("\n\n Destruidores: %d\n Submarinos--: %d\n Torepedos---: %d\n",d,s,t);
+}
+
+// --showOcean--
+// apresenta o oceano após o lançamento de um torpedo. De acordo com o modo escolhido no MD
+
+void
+showOcean(char ocean[O][O])
+{
+    //int MD = 1;  // Selecionador de modo PLAY(0) / CORR(1)
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < O; i++)
+    {
+        printf("\n");
+
+        for(j = 0; j < O; j++)
+        {
+            if (i == 0)
+            {
+                printf(" %c  ", ocean[i][j]);
+            }
+            else
+            {
+                if(MD == 0)
+                {
+                    if(ocean[i][j] == 'S')
+                    {
+                        printf("   |");
+                    }
+                    else
+                    {
+                        printf(" %c |", ocean[i][j]);
+                    }
+                }
+                if(MD == 1)
+                {
+                    printf(" %c |", ocean[i][j]);
+                }
+            }
+        }
+
+        printf("\n   +---+---+---+---+---+");
+    }
+}
+
+// --initOcean--
+// inicia um oceano com espaço ('') em todas as posições
+
+void
+initOcean(char ocean[O][O])
+{
+    int i = 0;
+    int j = 0;
+
+
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            ocean[i][j] = ' ';
+        }
+    }
+
+    ocean[0][0] = ' ';
+
+    ocean[1][0] = 'A';
+    ocean[2][0] = 'B';
+    ocean[3][0] = 'C';
+    ocean[4][0] = 'D';
+    ocean[5][0] = 'E';
+
+    ocean[0][1] = '1';
+    ocean[0][2] = '2';
+    ocean[0][3] = '3';
+    ocean[0][4] = '4';
+    ocean[0][5] = '5';
+}
+
+// --argsOk--
+// verifica se os argumentos passados estão de acordo com o que se espera no programa
+
+int
+argsOk (int argc, char *argv[])
+{
+    if(argc < 5)
+    {
+        return 1;
+    }
+    if((((strcmp(argv[1], "-m") == 0) && (strcmp(argv[3], "-t") == 0))))
+    {
+        if((strcmp(argv[2], "PLAY") != 0) && (strcmp(argv[2], "CORR") != 0))
+        {
+            return 3;
+        }
+    }
+    else if( (((strcmp(argv[1], "-t") == 0) && (strcmp(argv[3], "-m") == 0))) )
+    {
+        if((strcmp(argv[4], "PLAY") != 0) && (strcmp(argv[4], "CORR") != 0))
+        {
+            return 3;
+        }
+    }
+    else if( ((strcmp(argv[1], "-t")) == 0) && ((strcmp(argv[3], "-m")) != 0))
+    {
+        return 2;
+    }
+    else if( ((strcmp(argv[1], "-m")) == 0) && ((strcmp(argv[3], "-t")) != 0))
+    {
+        return 2;
+    }
+    else if( ((strcmp(argv[1], "-t")) != 0) && ((strcmp(argv[3], "-m")) == 0))
+    {
+        return 2;
+    }
+    else if( ((strcmp(argv[1], "-m")) != 0) && ((strcmp(argv[3], "-t")) == 0))
+    {
+        return 2;
+    }
+
+    else if( ((strcmp(argv[1], "-m")) != 0) && ((strcmp(argv[3], "-t")) != 0))
+    {
+        if( ((strcmp(argv[1], "-t")) != 0) && ((strcmp(argv[3], "-m")) != 0))
+        {
+            return 2;
+        }
+    }
+
+    if(isdigit(argv[2]) == 0)
+    {
+        if(isdigit(argv[4]) != 0)
+        {
+            return 3;
+        }
+    }
+
+
+    return 0;
+}
+
+// --errorMsg--
+// apresenta em stderr com uma mensagem de texto descrevendo o erro de acordo com o valor de x
+
+void
+errorMsg(int x)
+{
+    if(x == 1)
+    {
+	fprintf(stderr, "Faltam argumentos\n");
+    }
+    else if(x == 2)
+    {
+	fprintf(stderr, "Valores invalidos\n");
+    }
+    else if(x == 3)
+    {
+	fprintf(stderr, "O valor passado nao e um digito inteiro\n");
+    }
+    else
+    {
+	fprintf(stderr, "Aconteceu algum erro\n");
+    }
+}
+
+// --mantemOcean--
+// Mantem os valores que precisam ser lidos no oceano
+
+void
+mantemOcean(char ocean[O][O])
+{
+    ocean[0][0] = ' ';
+
+    ocean[1][0] = 'A';
+    ocean[2][0] = 'B';
+    ocean[3][0] = 'C';
+    ocean[4][0] = 'D';
+    ocean[5][0] = 'E';
+
+    ocean[0][1] = '1';
+    ocean[0][2] = '2';
+    ocean[0][3] = '3';
+    ocean[0][4] = '4';
+    ocean[0][5] = '5';
+
+}
